fbf06575a15e429d79bcf38e786f72397f2da043
Woisol-G <Woisol-G@outlook.com>
2024-02-01 20:11:48 +0800

    ！！！终于是实现了添加功能！！！历经千辛万苦呜呜呜呜呜呜 另外同时完善了控制台的功能，补上了帮助以及查询监视名单，黑名单，上一条记录的功能。当然大头还是前者…… 另外发现drop功能还有问题，对于已经加入的进行drop会异常，同时代码也有明显的书写错误 现在在外面用ToDesk写的，先提交了 不对似乎成了的只是之前把flomo删掉了导致没有模板才出错的……回去检测一下可以用了！！

--- obj/Debug/net8.0/MySQL Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/MySQL Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySQL Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4df2c8819f167b8e01f4dccb9d393535ccf4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93864a58944470c8a2069bcab697fe303db8b7e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySQL Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySQL Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/HUC_BackEnd.cs
+++ src/HUC_BackEnd.cs
@@ -4,6 +4,7 @@ using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Formats.Asn1;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 using MySqlConnector;//！这句开始无法识别要在命令行：dotnet add package MySqlConnector
 
 namespace MySqlTestSpace
@@ -14,7 +15,7 @@ namespace MySqlTestSpace
 		//**----------------------------aka ConsoleControl-----------------------------------------------------
 		{
 			Console.WriteLine("ConsoleControl Started, type your command below:");
-			new Thread(ProcessControl.mainLoop_Monitor).Start();//！用新线程的方法！注意没有()
+			(ProcessControl.threadMonitor = new Thread(ProcessControl.mainLoop_Monitor)).Start();//！用新线程的方法！注意没有()
 			//~~ threadMonitor.Start();
 			Console.WriteLine("Monitor running...");
 			while(true)
@@ -22,45 +23,56 @@ namespace MySqlTestSpace
 				// Thread.Sleep(20);//!要不然初始的存在信息就覆盖在>后面了
 				// Console.Write(">");
 				var input = Console.ReadLine();
-				if (input == "exit")
+				if (input.ToLower() == "exit")
 				{
 					//td输出，以及下面
 					Console.WriteLine("Exiting...");
-					ProcessControl.isMonitor = false;
+					ProcessControl.setMonitorStauts(false);
 					break;
+				}
+				else if(input.ToLower()  == "help")
+				{
+					Console.WriteLine(@"1.monitor on/off: set monitor on/off
+2.show apps: show apps monitored
+3.show blist: show blacklist
+4.show [app name]: show [app name] runtime");
 				}
 				else if(input.ToLower() == "monitor on")
 				{
 					Console.WriteLine("Opening Monitor");
-					ProcessControl.isMonitor = true;
-					new Thread(ProcessControl.mainLoop_Monitor).Start();////!艹不能重启啊…………那我搞变量有什么用…………
+					ProcessControl.setMonitorStauts(true);
 				}
 				else if(input.ToLower() == "monitor off")
 				{
 					Console.WriteLine("Closing Monitor");
-					ProcessControl.isMonitor = false;
+					ProcessControl.setMonitorStauts(false);
 				}
-				else if(new Regex(@"(?<=show )\w+").IsMatch(input))
+				else if(new Regex(@"(?<=show )\w+", RegexOptions.IgnoreCase).IsMatch(input))
 				{
-					var res = new Regex(@"(?<=show )\w+", RegexOptions.IgnoreCase).Match(input).Value;
-					//！忽略大小写的方法！
-					MySqlConnection sqlCon = new MySqlConnection($"Server=localhost;user=root;Database={ProcessControl.DATABASE_NAME};port=3306;password=60017089;");
-					sqlCon.Open();
-					try
+					if (input.ToLower() == "show apps")
 					{
-						MySqlDataReader sqlReader =  new MySqlCommand($"SELECT * FROM {res} ORDER BY StartTime DESC LIMIT 1;",sqlCon).ExecuteReader();
-						sqlReader.Read();//！当前上下文中不存在名称“sqlReader”看来try里面是一个密闭空间…………
-						//td输出
-						Console.WriteLine($"{res} current runtime: {sqlReader.GetDateTime("StartTime")} - {sqlReader.GetDateTime("EndTime")} for {sqlReader.GetDecimal("LastTime")}min");
+						Console.WriteLine("Apps Monitored:");
+						ProcessControl.showAppMonitored();
 					}
-					catch(Exception e)
+					else if (input.ToLower() == "show blist")
 					{
-						//td输出异常
-						Console.WriteLine(e.Message);
-						continue;
+						Console.WriteLine("Blacklist:");
+						ProcessControl.showBlackList();
 					}
-					//！sqlReader["StartTime"]和sqlReader.GetString("EndTime")的区别在于前者不知道返回类型返回的是object
-					sqlCon.Close();
+					else if (new Regex(@"(?<=show )\w+", RegexOptions.IgnoreCase).IsMatch(input))
+					{
+						var pcsName = new Regex(@"(?<=show )\w+", RegexOptions.IgnoreCase).Match(input).Value;
+						//！忽略大小写的方法！
+						ProcessControl.showPcsRunTime(pcsName);
+					}
+				}
+				else if(new Regex(@"(?<=add )\w+", RegexOptions.IgnoreCase).IsMatch(input))
+				{
+					ProcessControl.addAppMonitored(new Regex(@"(?<=add )\w+", RegexOptions.IgnoreCase).Match(input).Value);
+				}
+				else if(new Regex(@"(?<=drop )\w+", RegexOptions.IgnoreCase).IsMatch(input))
+				{
+					ProcessControl.dropAppMonitored(new Regex(@"(?<=drop )\w+", RegexOptions.IgnoreCase).Match(input).Value);
 				}
 				else{ Console.WriteLine("Unknown command"); }
 				// switch(input)还是不用switch了……效率估计不行
@@ -71,15 +83,13 @@ namespace MySqlTestSpace
 			// catch { }
 			// ProcessList.Add(Process.GetProcessesByName("code.exe")[0]);//！注意这里如果找不到会报错IndexOutOfRange
 			// !ProcessList.Add(new Process = Process.GetProcessesByName("code.exe")[0]);
-
-
 		}
 	}
 	class ProcessLog
 	{
-		public DateTime? startTime { get; set; } = null;
-		public DateTime? endTime { get; set; } = null;
-		public bool isRunning { get; set; } = false;
+		internal DateTime? startTime { get; set; } = null;
+		internal DateTime? endTime { get; set; } = null;
+		internal bool isRunning { get; set; } = false;
 	}
 	class ProcessControl
 	{
@@ -93,12 +103,17 @@ namespace MySqlTestSpace
 		internal string pcsName = "";
 
 		internal ProcessLog curPcs = new ProcessLog();
+		internal static List<ProcessControl> pcsMntList = new List<ProcessControl>();//~~~ = { new ProcessControl("Code"), new ProcessControl("flomo") };//!所以这个依然是应用名不过不要exe而已
+		internal static List<string> pcsMntBlackList = new List<string>();
 		internal static MySqlConnection sqlCon = new MySqlConnection($"server=localhost;user=root;database={DATABASE_NAME};port=3306;password=60017089");
 		// internal static MySqlCommand sqlCmd = new MySqlCommand();
 		internal static MySqlDataReader? sqlReader = null;
+		internal static Thread threadMonitor = new Thread(ProcessControl.mainLoop_Monitor);
 		internal static void mainLoop_Monitor()//!async X！注意这不是个标志，是在不能阻塞的方法里用，在里面用await来实现异步？
 		{
 			//~~ List<ProcessLog> processLogList = new List<ProcessLog>();
+			// if (sqlCon.State == ConnectionState.Open)
+			// 	sqlCon.Close();
 			sqlCon.Open();//！别忘艹…………而且不能多次调用不然出错
 			//~~ sqlCmd.Connection = sqlCon;
 			// List<ProcessControl> pcsMntList = new List<ProcessControl> { new ProcessControl("Code"), new ProcessControl("flomo") };
@@ -107,28 +122,134 @@ namespace MySqlTestSpace
 			//!md不搞一个静态cmd了…………巨麻烦艹
 			sqlReader = new MySqlCommand($"SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_SCHEMA = '{DATABASE_NAME}';", sqlCon).ExecuteReader();
 			// sqlCon.Close();
-			List<ProcessControl> pcsMntList = new List<ProcessControl>();//~~~ = { new ProcessControl("Code"), new ProcessControl("flomo") };//!所以这个依然是应用名不过不要exe而已
 			while(sqlReader.Read())
 			{
-				pcsMntList.Add(new ProcessControl(sqlReader.GetString(0)));
+				var curPcsName = sqlReader.GetString(0);
+				var isAdd = true;
+				if (pcsMntBlackList.Contains(curPcsName)) break;
+				foreach(var curPcs in pcsMntList)
+				{
+					if (curPcs.pcsName.ToLower() == curPcsName.ToLower()) isAdd = false;
+					//！md所以为什么两次读取表名是不一样的？？？？啊啊啊！！！！
+					//!考虑还是用ini文件等记录黑名单
+				}
+				if(isAdd)
+				pcsMntList.Add(new ProcessControl(curPcsName));
 			}
 			//!由于不能反复设置cmd的原因这里必须要重置一次…………
 			// sqlCmd.CommandText = null;并不行
 			sqlCon.Close();sqlCon.Open();//！关键句…………不理解艹…………
-			// sqlCmd = new MySqlCommand();
-			// sqlCmd.Connection = sqlCon;
-
 			for(;isMonitor ; Thread.Sleep(100))//
 			foreach(ProcessControl pcsCon in pcsMntList)
 			{
 				pcsCon.getPcsStatus();
 			}
 			//!先阻塞吧感觉好像也行
-			//td加一个check，如果两次关闭开启小于3min就不多记一次了
 			//td考虑电脑睡眠后的处理
-			sqlCon.Close();//!!暂时无法访问
+			sqlCon.Close();
 		}
-		//~~ internal static void showPcsRunTime(string pcsName)
+		internal static void setMonitorStauts(bool isMonitor)
+		{
+			if(isMonitor)
+			{
+				ProcessControl.isMonitor = true;
+				(threadMonitor = new Thread(ProcessControl.mainLoop_Monitor)).Start();////!艹不能重启啊…………那我搞变量有什么用…………
+			}
+			else
+			{
+				ProcessControl.isMonitor = false;
+				//这种方式其实不太正规吧…………
+				// if (threadMonitor != null && threadMonitor.IsAlive)
+			    threadMonitor.Join(); //！重要方法！！！ 等待结束否则connection没有close就打开无法加载新的command！
+				//！后面出现了重复检测的问题，开始一直以为是线程没关掉但是已经关了，在于List的Add加多了！！！！！！！！！！！！！
+			}
+		}
+		internal static void showPcsRunTime(string pcsName)
+		{
+			// MySqlConnection sqlCon = new MySqlConnection($"Server=localhost;user=root;Database={ProcessControl.DATABASE_NAME};port=3306;password=60017089;");
+			// sqlCon.Open();
+			try
+			{
+				MySqlDataReader sqlReader =  new MySqlCommand($"SELECT * FROM {pcsName} ORDER BY StartTime DESC LIMIT 1;",sqlCon).ExecuteReader();
+				sqlReader.Read();//！当前上下文中不存在名称“sqlReader”看来try里面是一个密闭空间…………
+				//td输出
+				Console.WriteLine($"{pcsName} current runtime: {sqlReader.GetDateTime("StartTime")} - {sqlReader.GetDateTime("EndTime")} for {sqlReader.GetDecimal("LastTime")}min");
+			}
+			catch(Exception e)
+			{
+				//td输出异常
+				Console.WriteLine(e.Message);
+				return;
+			}
+			//！sqlReader["StartTime"]和sqlReader.GetString("EndTime")的区别在于前者不知道返回类型返回的是object
+			sqlCon.Close();
+
+		}
+		internal static void showAppMonitored()
+			{
+					if(pcsMntList.Count == 0)
+			{
+				//td输出
+				Console.WriteLine("No app monitored!");
+			}
+			foreach(ProcessControl pcsCon in pcsMntList)
+			{
+				//td输出
+				Console.WriteLine(pcsCon.pcsName);
+			}
+		}
+		internal static void showBlackList()
+		{
+			if(pcsMntList.Count == 0)
+			{
+				//td输出
+				Console.WriteLine("No app in black list!");
+			}
+			foreach(string blackName in pcsMntBlackList)
+			{
+				//td输出
+				Console.WriteLine(blackName);
+			}
+		}
+		internal static void addAppMonitored(string pcsName)
+		{
+			// pcsMntList.ForEach(pcsCon => if(pcsCon.pcsName == pcsName)throw new Exception("App already monitored!");)
+			//！wokforeach的行内写法！！！可惜if用不了
+			if (pcsMntBlackList.Contains(pcsName))
+			pcsMntBlackList.Remove(pcsName);
+			foreach(ProcessControl pcsCon in pcsMntList)
+			{
+				if(pcsCon.pcsName == pcsName)
+				// throw new Exception("App already monitored!");
+				{
+					Console.WriteLine("App already monitored!");return;
+				}
+			}
+			new MySqlCommand($"CREATE TABLE IF NOT EXISTS {pcsName} LIKE flomo;", sqlCon).ExecuteNonQuery();//!艹这里不能用异步了…………
+			//td新建一个样板表
+			new MySqlCommand($"INSERT INTO {pcsName} VALUES ('{DateTime.Now}', '{DateTime.Now}', 0);", sqlCon).ExecuteNonQueryAsync();
+			//!可以多次cmd…………
+			//！不能在foreach的时候改变List，所以只能重启进程了………………
+			setMonitorStauts(false);
+			Console.WriteLine("Rebooting...");
+			pcsMntList.Add(new ProcessControl(pcsName));
+			setMonitorStauts(true);
+		}
+		internal static void dropAppMonitored(string pcsName)
+		{
+			foreach(ProcessControl pcsCon in pcsMntList)
+			{
+				if(pcsCon.pcsName == pcsName)
+				{
+					pcsMntList.Remove(pcsCon);
+					//td输出
+					Console.WriteLine("App dropped!");
+					break;
+				}
+			}
+			if (pcsMntBlackList.Contains(pcsName)) {Console.WriteLine("App already dropped!");}//td输出
+			else pcsMntBlackList.Add(pcsName);
+		}
 		void getPcsStatus()
 		//**理清思路
 		//未启动：null, null, false				where Lengh = 0
@@ -169,10 +290,10 @@ namespace MySqlTestSpace
 
 					MySqlConnection lastCon = new MySqlConnection($"server=localhost;user=root;database={DATABASE_NAME};port=3306;password=60017089");
 					lastCon.Open();//麻了又忘
-					MySqlCommand lastCmd = new MySqlCommand($"SELECT * FROM {pcsName} ORDER BY EndTime DESC LIMIT 1;", lastCon);
-					MySqlDataReader lastDataReader = lastCmd.ExecuteReader();
+					MySqlDataReader lastDataReader = new MySqlCommand($"SELECT * FROM {pcsName} ORDER BY EndTime DESC LIMIT 1;", lastCon).ExecuteReader();
 					lastDataReader.Read();
 					// var lastEndTime = lastDataReader.GetDateTime("EndTime");
+					//！新建了一个app table但是没有初始数据导致异常！
 					if((DateTime.Now - lastDataReader.GetDateTime("EndTime")).TotalMinutes < 5 )
 					{
 						//~~ this.curPcs.startTime = lastDataReader.GetDateTime("StartTime");
@@ -193,8 +314,8 @@ namespace MySqlTestSpace
 						this.curPcs.startTime = DateTime.Now.ToLocalTime();
 						//~~ sqlCmd.CommandText = $"INSERT INTO {pcsName} VALUES ('{this.curPcs.startTime}', '{this.curPcs.startTime}', 0);";
 						new MySqlCommand($"INSERT INTO {pcsName} VALUES ('{this.curPcs.startTime}', '{this.curPcs.startTime}', 0);",sqlCon).ExecuteNonQueryAsync();
+						//INSERT INTO AltDrag VALUES ('2024-01-01 00:00:00', '2024-01-01 00:00:00', 0);
 						Console.WriteLine($"{pcsName} start at {this.curPcs.startTime}");//!ToString("yyyy-MM-dd HH:mm:ss")}但是似乎不太行
-
 					}
 				}
 				//!要考虑到如果长时间运行数据完全不更新…………
